3b13d37beef42547438599e06ebb6c6a93a52410
Cory Powell <coryrpowell@gmail.com>
2023-05-17 16:24:22 -0400

    add arrays

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+#Arrays
+
+##Strings
+first_names = ["Bob", "Frank", "Coop", "Laura",]
+
+
+ages = [21, 22, 23, 24]
+
+
+power_levels = [9000.5, 500.25, 4500.30, 5550.50]
+
+
+are_dudes = [true, true, true, false]
