824d95147aae6980bec859ded32a8af8984f2bbf
Cory Powell <coryrpowell@gmail.com>
2023-05-17 16:37:48 -0400

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,6 +1,3 @@
-#Arrays
-
-##Strings
 first_names = ["Bob", "Frank", "Coop", "Laura",]
 
 
@@ -11,3 +8,22 @@ power_levels = [9000.5, 500.25, 4500.30, 5550.50]
 
 
 are_dudes = [true, true, true, false]
+
+#the pop method will remove the last element so Laura would be removed.
+first_names.pop
+p first_names
+
+
+#the push method adds an element to the end of the array, so it would add 25 to the end of the array.
+ages.push(25)
+p ages
+
+
+#the shift method removes the first element in an array so this would remove 9000.5.
+power_levels.shift
+p power_levels
+
+
+#the unshift method adds a new element to the beginning of the array so this would add a false at the beginning of the array.
+are_dudes.unshift(false)
+p are_dudes
